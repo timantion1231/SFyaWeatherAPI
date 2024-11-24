394d51b8e83262e74ced2fdd1512275ba1d73607
Timur Khanafin <mindiisky@ya.ru>
2024-11-24 21:21:36 +0300

    Получение всех данных yandexWeatherAPI

--- SFyaWeatherAPI/src/main/java/org/example/Main.java
+++ SFyaWeatherAPI/src/main/java/org/example/Main.java
@@ -1,7 +1,54 @@
 package org.example;
 
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Properties;
+
 public class Main {
+    private static String apiKey = null;
+    private static final String API_URL = "https://api.weather.yandex.ru/v2/forecast";
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        URL url;
+        HttpURLConnection connection = null;
+        StringBuilder content;
+
+        Properties properties = new Properties();
+
+        try (FileInputStream input = new FileInputStream("config.properties")) {
+            properties.load(input);
+            apiKey = properties.getProperty("api.key");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            url = new URL(API_URL);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setRequestProperty("X-Yandex-Weather-Key", apiKey);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            content = new StringBuilder();
+
+            while ((inputLine = in.readLine()) != null) {
+                content.append(inputLine);
+            }
+            in.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            connection.disconnect();
+        }
+        System.out.println(content);
     }
+
 }
+
+
